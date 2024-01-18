6689e28dd7b29ad255792363b8d630ef8c0edd42
Piyush <xyz@gmail.com>
2024-01-18 21:43:13 +0530

    First commit

---
+++ 1D-Array_1.cpp
@@ -0,0 +1,126 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+//*******************************************************************************************************************//
+
+    // int n;
+    // cout<<"Enter the no of student: ";
+    // cin>>n;
+    // int arr[n];
+
+    // cout<<"Enter the marks: "<<endl;
+    // for(int i=0; i<n; i++){
+    //     cin>>arr[i];
+    // }
+    // for(int i=0; i<n; i++){
+    //     if(arr[i] < 35){
+    //         cout<<i<<endl;
+    //     }
+    // }
+
+//*******************************************************************************************************************//
+
+    // int arr1[] = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,5,4,43,3,3,2,2};
+    // int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    // float arr2[] = {1.1,2.2,3.3,4.4,5.5,6.6};
+    // int size2 = sizeof(arr2)/sizeof(arr2[0]);
+    // cout<<size1<<endl;
+    // cout<<size2<<endl;
+
+//*******************************************************************************************************************//
+
+    // int arr[5] = {1,2,3,4,5};
+    // cout<<"Adress of all the elements: "<<endl;
+    // for(int i=0; i<5; i++){
+    //     cout<<&arr[i]<<endl;
+    // }
+    // cout<<endl;
+
+    // cout<<"Adress of first element: "<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<&arr<<endl;
+    // cout<<arr<<endl;
+
+//*******************************************************************************************************************//
+
+    // int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    // int sum = 0;
+    // for(int i=0; i<10; i++){
+    //     sum += arr[i];
+    // }
+    // cout<<"The sum of all the elements in the array: "<<sum<<endl;
+
+//*******************************************************************************************************************//
+
+    // int arr[10] = {1,2,3,4,4,5,6,4,3,2};
+    // int x;
+    // cout<<"Enter the element you want to search: ";
+    // cin>>x;
+    // bool flag = false;
+    // for(int i=0; i<10; i++){
+    //     if(arr[i] == x){
+    //         flag = true;
+    //         break;
+    //     }
+    // }
+    // if(flag == true){
+    //     cout<<x<<" is presnt at position: "<<endl;
+    //     for(int i=0; i<10; i++){
+    //         if(arr[i] == x){
+    //             cout<<i+1<<endl;    // i+1 because we want position not index
+    //         }
+    //     }
+    // }
+    // else{
+    //     cout<<"Element is absent in the array "<<endl;
+    // }
+
+//*******************************************************************************************************************//
+
+    // int arr[6] = {100,200,400,400,300,400};
+    // int max = INT_MIN;
+    // for(int i=0; i<6; i++){
+    //     if(max < arr[i]){
+    //         max = arr[i];
+    //     }
+    // }
+    // cout<<"Maximum element in the array: "<<max<<endl;
+
+    // int smax = INT_MIN;
+    // for(int i=0; i<6; i++){
+    //     if(smax < arr[i] && arr[i] != max){
+    //         smax = arr[i];
+    //     }
+    // }
+    // cout<<"Second Maximum element in the array: "<<smax<<endl;
+
+//*******************************************************************************************************************//
+
+    // int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    // int x;
+    // cout<<"Enter the numeber: ";
+    // cin>>x;
+    // bool flag = false;
+    // for(int i=0; i<10; i++){
+    //     if(arr[i] > x){
+    //         flag = true;
+    //         break;
+    //     }
+    // }
+    // if(flag == true){
+    //     cout<<"Element greater than "<<x<<" is/are present at position: "<<endl;
+    //     for(int i=0; i<10; i++){
+    //         if(arr[i] > x){
+    //             cout<<i+1<<endl;   // i+1 because we want position not index
+    //         }
+    //     }
+    // }
+    // else{
+    //     cout<<"There is no element greater than "<<x<<endl;
+    // }
+
+//*******************************************************************************************************************//
+
+    return 0;
+}
